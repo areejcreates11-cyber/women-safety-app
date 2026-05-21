80c228cc3a1872381da82712938a602e5e17cb60
areejcreates11-cyber <areejcreates11@gmail.com>
2026-05-21 19:02:44 +0500

    Add files via upload

---
+++ Dashboard.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace safetyapp
+{
+    partial class Dashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnHome = new System.Windows.Forms.Button();
+            this.btnManageContacts = new System.Windows.Forms.Button();
+            this.btnLogout = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnSOS = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(32)))), ((int)(((byte)(43)))));
+            this.panel1.Controls.Add(this.btnHome);
+            this.panel1.Controls.Add(this.btnManageContacts);
+            this.panel1.Controls.Add(this.btnLogout);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(158, 450);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint_1);
+            // 
+            // btnHome
+            // 
+            this.btnHome.BackColor = System.Drawing.Color.Transparent;
+            this.btnHome.FlatAppearance.BorderSize = 0;
+            this.btnHome.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHome.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHome.ForeColor = System.Drawing.Color.White;
+            this.btnHome.Location = new System.Drawing.Point(14, 38);
+            this.btnHome.Name = "btnHome";
+            this.btnHome.Size = new System.Drawing.Size(104, 61);
+            this.btnHome.TabIndex = 1;
+            this.btnHome.Text = "Home";
+            this.btnHome.UseVisualStyleBackColor = false;
+            this.btnHome.Click += new System.EventHandler(this.btnHome_Click);
+            // 
+            // btnManageContacts
+            // 
+            this.btnManageContacts.BackColor = System.Drawing.Color.Transparent;
+            this.btnManageContacts.FlatAppearance.BorderSize = 0;
+            this.btnManageContacts.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnManageContacts.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnManageContacts.ForeColor = System.Drawing.Color.White;
+            this.btnManageContacts.Location = new System.Drawing.Point(14, 159);
+            this.btnManageContacts.Name = "btnManageContacts";
+            this.btnManageContacts.Size = new System.Drawing.Size(121, 66);
+            this.btnManageContacts.TabIndex = 2;
+            this.btnManageContacts.Text = "Emergency Contacts";
+            this.btnManageContacts.UseVisualStyleBackColor = false;
+            this.btnManageContacts.Click += new System.EventHandler(this.btnManageContacts_Click);
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.BackColor = System.Drawing.Color.Transparent;
+            this.btnLogout.FlatAppearance.BorderSize = 0;
+            this.btnLogout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogout.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogout.ForeColor = System.Drawing.Color.White;
+            this.btnLogout.Location = new System.Drawing.Point(14, 294);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Size = new System.Drawing.Size(104, 69);
+            this.btnLogout.TabIndex = 3;
+            this.btnLogout.Text = "Logout";
+            this.btnLogout.UseVisualStyleBackColor = false;
+            this.btnLogout.Click += new System.EventHandler(this.btnLogout_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(244)))), ((int)(((byte)(248)))));
+            this.panel2.Controls.Add(this.btnSOS);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(158, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(526, 450);
+            this.panel2.TabIndex = 1;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // btnSOS
+            // 
+            this.btnSOS.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(225)))), ((int)(((byte)(40)))), ((int)(((byte)(60)))));
+            this.btnSOS.FlatAppearance.BorderSize = 0;
+            this.btnSOS.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(65)))), ((int)(((byte)(85)))));
+            this.btnSOS.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSOS.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSOS.ForeColor = System.Drawing.Color.White;
+            this.btnSOS.Location = new System.Drawing.Point(75, 142);
+            this.btnSOS.Name = "btnSOS";
+            this.btnSOS.Size = new System.Drawing.Size(370, 164);
+            this.btnSOS.TabIndex = 0;
+            this.btnSOS.Text = "SEND SOS";
+            this.btnSOS.UseVisualStyleBackColor = false;
+            this.btnSOS.Click += new System.EventHandler(this.btnSOS_Click);
+            // 
+            // Dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Dashboard";
+            this.Text = "Dashboard";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnSOS;
+        private System.Windows.Forms.Button btnHome;
+        private System.Windows.Forms.Button btnManageContacts;
+        private System.Windows.Forms.Button btnLogout;
+    }
+}

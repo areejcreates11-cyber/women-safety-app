53a1380d64bd84dde694e3284e71801027d4b84b
areejcreates11-cyber <areejcreates11@gmail.com>
2026-05-21 18:59:15 +0500

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,9 @@
+﻿IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'EmergencyContacts')
+BEGIN
+    CREATE TABLE EmergencyContacts (
+        Id INT PRIMARY KEY IDENTITY(1,1),
+        UserId INT,
+        ContactName NVARCHAR(100),
+        ContactPhone NVARCHAR(20)
+    );
+END
